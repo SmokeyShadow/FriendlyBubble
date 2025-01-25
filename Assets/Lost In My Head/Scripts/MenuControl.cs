39fdc25bb6623910571214b8243de4dc0280b9c9
SmokeyShadow <atiehnorouzi22@gmail.com>
2025-01-25 18:19:13 +0330

    -Add menu

---
+++ Assets/Lost In My Head/Scripts/MenuControl.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuControl : MonoBehaviour
+{
+    #region SERIALIZED FIELDS
+    [SerializeField]
+    private GameObject howtoPlay;
+
+    [SerializeField]
+    private GameObject startPanel;
+
+    [SerializeField]
+    private Button startBtn;
+
+    [SerializeField]
+    private Button howtoBtn;
+
+    [SerializeField]
+    private Button quitBtn;
+
+    [SerializeField]
+    private Button backBtn;
+    #endregion
+
+    #region MONO BEHAVIOURS
+    void Start()
+    {
+        startBtn.onClick.AddListener(StartGame);
+        quitBtn.onClick.AddListener(QuitGame);
+        howtoBtn.onClick.AddListener(HowToPlay);
+        backBtn.onClick.AddListener(BackToStart);
+    }
+    #endregion
+
+    #region PRIVATE METHODS
+    void BackToStart()
+    {
+        howtoPlay.SetActive(false);
+        startPanel.SetActive(true);
+    }
+
+    void HowToPlay()
+    {
+        howtoPlay.SetActive(true);
+        startPanel.SetActive(false);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
